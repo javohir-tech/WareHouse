560ec90358f7a88a5cdb210b311f12221394df81
Suvonov Javohir <suvonovjavohir625@gmail.com>
2026-01-18 18:33:31 +0500

    update

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SingUpView, CodeVerifyView
+from .views import SingUpView, CodeVerifyView , CodeVerifyRestView
 
 urlpatterns = [
     path("singup/", SingUpView.as_view()),
     path("code_verify/", CodeVerifyView.as_view()),
+    path("code_rest/" , CodeVerifyRestView.as_view())
 ]
--- users/views.py
+++ users/views.py
@@ -4,11 +4,14 @@ from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from .serializer import SingUpSerializer, CodeVerifySerializer
 from rest_framework.response import Response
-from .models import User, AuthStatus
+from .models import User, AuthStatus, AuthType
 from .permissions import IsRegistrationPermissions, IsVerifyCodePermission
 from .authentication import RegistrationTokenAuthentication
 from .tokens import RegistrationToken
 from rest_framework.exceptions import ValidationError
+from django.utils import timezone
+from rest_framework import status
+from shared.utils import send_email
 
 
 class SingUpView(generics.CreateAPIView):
@@ -23,7 +26,9 @@ class CodeVerifyView(APIView):
 
     def post(self, request, *args, **kwargs):
         user = self.request.user
-        serializer = CodeVerifySerializer(data=request.data, context={"user": self.request.user})
+        serializer = CodeVerifySerializer(
+            data=request.data, context={"user": self.request.user}
+        )
         if serializer.is_valid(raise_exception=True):
             user.auth_status = AuthStatus.DONE
             return Response(
@@ -35,10 +40,50 @@ class CodeVerifyView(APIView):
                 }
             )
         else:
+            raise ValidationError({"success": False, "message": str(serializer.errors)})
+
+
+class CodeVerifyRestView(APIView):
+    permission_classes = [IsRegistrationPermissions, IsVerifyCodePermission]
+    authentication_classes = [RegistrationTokenAuthentication]
+
+    def get(self, request, *args, **kwargs):
+        user = self.request.user
+
+        if user is None:
+            return Response(
+                {"success": False, "message": "Siz ro'yhatdan o'tmagansiz"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        self.check_code(user)
+
+        if user.auth_type == AuthType.VIA_EMAIL:
+            user_code = user.code_verify(AuthType.VIA_EMAIL)
+            send_email(user.email, user_code)
+        elif user.auth_type == AuthType.VIA_PHONE:
+            user_code = user.code_verify(AuthType.VIA_PHONE)
+            send_email(to_email="suvonovjavohir625@gmail.com", code=user_code)
+
+        return Response(
+            {
+                "success": True,
+                "message": "Code yuborildi",
+                "registration_token": str(RegistrationToken.for_user(user)),
+            },
+            status=status.HTTP_200_OK,
+        )
+
+    @staticmethod
+    def check_code(user):
+        verify = user.verify_codes.filter(
+            expiration_time__gte=timezone.now(), is_confirmed=False
+        )
+        if verify.exists():
             raise ValidationError(
                 {
-                    "success" : False,  
-                    "message" : str(serializer.errors)
+                    "success": False,
+                    "message": "siz hozirda yaroqli kod bor biroz kuting",
                 }
             )
 
